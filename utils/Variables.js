73195e8a13b7e1cf1fab0a99478f7fd2eaee10a7
OskarWiiala <pullumastery@gmail.com>
2021-01-29 09:07:18 +0200

    updated baseURL and uploadsURL

--- utils/Variables.js
+++ utils/Variables.js
@@ -1,4 +1,4 @@
-const uploadsURL = 'http://media.mw.metropolia.fi/wbma/uploads/';
-const baseUrl = 'http://media.mw.metropolia.fi/wbma/';
+const uploadsURL = 'http://media-new.mw.metropolia.fi/wbma/uploads/';
+const baseUrl = 'http://media-new.mw.metropolia.fi/wbma/';
 
 export {uploadsURL, baseUrl};
